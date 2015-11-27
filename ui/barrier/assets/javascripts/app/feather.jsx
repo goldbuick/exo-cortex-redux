7f3e6cd694f751ed31a5a0511e52061e00eb6a32
David Whitlark <whitlark@gmail.com>
2015-11-27 04:54:01 -0500

    cleanup

--- ui/barrier/assets/javascripts/app/feather.jsx
+++ ui/barrier/assets/javascripts/app/feather.jsx
@@ -18,14 +18,7 @@ var Feather = React.createClass({
 
         let center = 800,
             width = 16,
-            edge = 128,
-            step = 8,
-            front = 64,
-            drift = 40,
-            base = 23,
-            segs = 256,
-            thresh = 0.3,
-            rings = 8;
+            edge = 128;
 
         edge = width;
         for (let i=0; i<32; ++i) {
@@ -35,36 +28,6 @@ var Feather = React.createClass({
             feather.drawSwipe(0, 0, z, 128, center + edge, width - 2, arc - twist, arc + twist, -12);
             edge += width;
         }
-
-        // feather.drawLoopR(0, 0, 0, 128, center - 32, r, 0.3, base, base);
-
-        // edge = 128;
-        // feather.drawLoopR(0, 0, 0, 128, center + edge, r, 0.3, base, base);
-
-        // for (let i=0; i<rings; ++i) {
-        //     let z = -64;
-        //     edge += step;
-        //     feather.drawLoopR(0, 0, z, segs, center + edge, r, thresh,
-        //         front + Math.floor(r() * drift),
-        //         front - Math.floor(r() * drift));
-        //     edge += step;
-        //     feather.drawLoopR(0, 0, z, segs, center + edge, r, thresh,
-        //         front - Math.floor(r() * drift),
-        //         front + Math.floor(r() * drift));
-        //     edge += step * Math.floor(r() * 10);
-        // }
-
-        // for (let i=0; i<64; ++i) {
-        //     let a = r() * Math.PI * 2,
-        //         radius = center + (r() * edge),
-        //         x = Math.cos(a) * radius,
-        //         y = Math.sin(a) * radius;
-        //     radius = 8 + r() * 32;
-        //     for (let c=0; c<Math.floor(r() * 8); ++c) {
-        //         feather.drawLoop(x, y, 0, 6, radius);
-        //         radius += 8;
-        //     }
-        // }
 
         return feather.build({
             transform: Graph.projectFacePlane(1)
