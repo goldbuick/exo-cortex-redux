ce47486ef18a022db204e81f9b5340ad48b4295d
David Whitlark <whitlark@gmail.com>
2016-03-18 12:40:50 -0400

    calmness

--- stem/lib/_didact/RunDocker.js
+++ stem/lib/_didact/RunDocker.js
@@ -16,7 +16,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var RunDocker = (function (_Run) {
+var RunDocker = function (_Run) {
     _inherits(RunDocker, _Run);
 
     function RunDocker(host, port) {
@@ -26,6 +26,6 @@ var RunDocker = (function (_Run) {
     }
 
     return RunDocker;
-})(_Run3.default);
+}(_Run3.default);
 
 exports.default = RunDocker;
--- ui/didact/assets/javascripts/lib/viz/Draft.jsx
+++ ui/didact/assets/javascripts/lib/viz/Draft.jsx
@@ -1,14 +1,49 @@
 import Etch from 'lib/viz/Etch';
 
 class Draft {
+
+    constructor () {
+        this.etch = new Etch();
+    }
+
+    tessellate (step) {
+        return this.etch.tessellate(step);
+    }
+
+    build (transform) {
+        return this.etch.build(transform);
+    }
+
+    hexPod (x, y, z, radius, count, step) {
+    	for (let i=0; i < count; ++i) {
+    		this.etch.drawLoop(x, y, z, 6, radius);
+    		radius += step;
+    	}
+    }
+
+    featherArcR (x, y, z, radius, count, step, r, width, depth, drift) {
+    	for (let i=0; i < count; ++i) {
+    		let _z = z + (i * -depth),
+    			twist = Math.floor((r() - 0.5) * 32),
+    			arc = 45 + Math.floor(r() * 20);
+    		this.etch.drawSwipe(x, y, _z, 128, radius, width, arc - twist, arc + twist, -drift);
+    		radius += width + step;
+    	}
+    }
+
+	// radial lines - maybe a line sequence along a path ?
+	// line edges, swipe edges - I want to draw a circle with multiple radius sizes 
+	// line sequences | | | | 
+	// repeated chevrons 
+
 }
 
 export default Draft;
 
 /* more complex structures 
 
-hex pods - from barrier
-feather arcs - from barrier
+recursive squares 
+
 
 diamond gridded backgrounds
 gridded backgrounds
@@ -19,10 +54,6 @@ hex fields ( each hex can be scalable )
 vector flows
 particle flows ? (next layer up from digram?)
 
-radial lines - maybe a line sequence along a path ?
-line edges, swipe edges - I want to draw a circle with multiple radius sizes 
-line sequences | | | | 
-repeated chevrons 
 
 linear auto-rigging for skeleton animation
 
@@ -31,6 +62,5 @@ map contours ??
 sphere shells - wtf do I mean here ?
 
 identi glyphs !!!
-recursive squares 
 
 */
